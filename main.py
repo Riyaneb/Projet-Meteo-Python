4e930167a5885145321396c838b2d7859ec44e76
Riyaneb <riyane.bouakaz@etu.cyu.fr>
2026-02-14 15:08:35 +0100

    Remplacement du csv par du sql

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 import reverse_geocoder as rg
-
+import sqlite3
 
 class relevéMétéo:
     def __init__(self,liste):
@@ -20,6 +20,15 @@ class relevéMétéo:
         else:
             return False
         
+def moyenne(liste):
+    cpt=0
+    n=0
+    for i in liste:
+        cpt += float(i)
+        n += 1
+    return cpt/n
+
+        
 API = "https://api.open-meteo.com/v1/forecast?latitude=48.8534,45.7485,43.297,44.8404,48.85,48.112,50.633,43.7031,43.6043,48.5839,47.2172&longitude=2.3488,4.8467,5.3811,-0.5805,2.6,-1.6743,3.0586,7.2661,1.4437,7.7455,-1.5534&hourly=temperature_2m&timezone=auto"
 r = requests.get(API)
 
@@ -28,39 +37,36 @@ if r.status_code == 200:
     if donnee == None:
         print("Donnée JSON non récupéré")
     else:
+        conn = sqlite3.connect('data.db')
+        c = conn.cursor()
+        c.execute('''CREATE TABLE IF NOT EXISTS Meteo
+                (Longitude real, Latitude real, Elevation real, Temperature real)''')
+        c.execute('''DELETE FROM Meteo''')
+        for ville in donnee:
+            liste = ville['hourly']['temperature_2m']
+            temp = moyenne(liste)
+            tmp = ('''INSERT INTO Meteo VALUES (?,?,?,?)''')
+            champ = (ville['longitude'],ville['latitude'],ville['elevation'],temp)
+            c.execute(tmp,champ)
+        conn.commit()
+
+        nb = 0
+        for i in c.execute('SELECT * FROM Meteo'):
+            Releve = relevéMétéo(i)
+            if Releve.Canicule():
+                print(f"Attention, canicule à {Releve.ville[0]['name']} : {Releve.temperature:.2f}C°")
+                nb += 1
+            elif Releve.Gele():
+                print(f"Attention, Ca gèle à {Releve.ville[0]['name']} : {Releve.temperature:.2f}C°")
+                nb += 1
+        if nb == 0:
+            print("Problème nulle part")
+ 
+
         
-        with open("data.csv", "w") as f:
-            f.write("Longitude,Latitude,Elevation,Temperature\n")
-            for ville in donnee:
-                liste = ville['hourly']['temperature_2m']
-                cpt=0
-                n=0
-                for i in liste:
-                    cpt += float(i)
-                    n += 1
-                moyenne = cpt/n
-                f.write(f"{ville['longitude']},{ville['latitude']},{ville['elevation']},{moyenne}\n")
-            
-        with open("data.csv", "r") as f:
-            next(f)
-            nb = 0
-            for ligne in f:
-                ligne = ligne.strip()
-                colonne = ligne.split(",")
-                Releve = relevéMétéo(colonne)
-                if Releve.Canicule():
-                    print(f"Attention, canicule à {Releve.ville[0]['name']} : {Releve.temperature:.2f}C°")
-                    nb += 1
-                elif Releve.Gele():
-                    print(f"Attention, Ca gèle à {Releve.ville[0]['name']} : {Releve.temperature:.2f}C°")
-                    nb += 1
-                
-            if nb != 0:
-                print("Problème nulle part")
+        conn.close()
 else:
     print("Probème avec l'api")
-
-
 
 
 
